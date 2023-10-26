51500103c6a81c7b2e6fe35f7a9349c64b39790b
Tsuyoshi Kadookura <39421054+kadokuratsuyoshi@users.noreply.github.com>
2023-10-27 06:29:21 +0900

    Add files via upload

---
+++ EMUZ80-6502/putc/putc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+    putchar(0x0d);
+    putchar(0x0a);
+    putchar('h');
+    putchar('e');
+    putchar('l');
+    putchar('l');
+    putchar('o');
+    putchar(',');
+    putchar('E');
+    putchar('M');
+    putchar('U');
+    putchar('Z');
+    putchar('8');
+    putchar('0');
+    putchar('-');
+    putchar('6');
+    putchar('5');
+    putchar('0');
+    putchar('2');
+    return 0;
+}
